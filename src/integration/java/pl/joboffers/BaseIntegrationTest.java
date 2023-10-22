faa5accd81dd68508e8dfa84b4e7f0ba5fcab314
Paweł Graczyk <pawgra7@gmail.com>
2023-10-22 08:37:21 +0200

    Added base integration tests structure.

---
+++ src/integration/java/pl/joboffers/BaseIntegrationTest.java
@@ -0,0 +1,9 @@
+package pl.joboffers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.joboffers.domain.JobOffersSpringBootApplication;
+
+@SpringBootTest(classes = JobOffersSpringBootApplication.class)
+public class BaseIntegrationTest {
+
+}
